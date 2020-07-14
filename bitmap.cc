a8be9f488379616515e05ff17ce3926bae468bc4
Juan De Leon <dele17822@uvg.edu.gt>
2020-07-13 20:23:49 -0600

    Bitmap Memory 2

--- bitmap.cc
+++ bitmap.cc
@@ -1,6 +1,6 @@
 #include "bitmap.h"
 
-Bitmap::Bitmap(int width, int height): _width(width), _height(height){
+Bitmap::Bitmap(int width, int height): _width(width), _height(height), m_pPixels(new uint8_t[width*height*3]){
     //Constructor
 
 }
