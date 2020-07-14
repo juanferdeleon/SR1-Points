cc755d2f1adb145b8c47e9cf7b5d6ac30d46f2a1
Juan De Leon <dele17822@uvg.edu.gt>
2020-07-13 20:34:25 -0600

    Header values

--- bitmap.cc
+++ bitmap.cc
@@ -1,11 +1,23 @@
 #include "bitmap.h"
+#include "bitmapinfoheader.h"
+#include "bitmapfileheader.h"
 
-Bitmap::Bitmap(int width, int height): _width(width), _height(height), m_pPixels(new uint8_t[width*height*3]){
+Bitmap::Bitmap(int width, int height): _width(width), _height(height), m_pPixels(new uint8_t[width*height*3]{}){
     //Constructor
 
 }
 
 bool Bitmap::write(string fileName){
+
+    BitmapFileHeader fileHeader;
+    BitmapInfoHeader infoHeader;
+
+    fileHeader.fileSize = sizeof(BitmapFileHeader) + sizeof(BitmapInfoHeader) + _width * _height * 3;
+    fileHeader.dataOffset = sizeof(BitmapFileHeader) + sizeof(BitmapInfoHeader);
+
+    infoHeader.width = _width;
+    infoHeader.height = _height;
+
     return false;
 }
 
