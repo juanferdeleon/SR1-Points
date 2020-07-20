010dd3b866326ef1c187a1f48c950ca0edf4b5ef
Juan De Leon <dele17822@uvg.edu.gt>
2020-07-20 13:25:09 -0600

    Bitmap class

--- SR1-Points Python/gl.py
+++ SR1-Points Python/gl.py
@@ -27,3 +27,54 @@ def color(r,g,b):
     '''Set pixel color'''
     return bytes([b, g, r])
 
+class Bitmap(object):
+    '''Bitmap Class'''
+
+    def __init__(self, heigh, width):
+        '''Constructor'''
+        self.heigh = heigh
+        self.width = width
+        self.frambufer = []
+        self.clear_color = color(0, 0, 0)
+        self.glClear()
+
+    def glInit(self):
+        pass
+
+    def glClear(self):
+        '''Set all pixels to same color'''
+        self.frambufer = [
+            [self.clear_color for x in range(self.width)] for y in range(self.heigh)
+        ]
+    
+    def glWrite(self, file_name):
+        '''Write Bitmap File'''
+        
+        bmp_file = open(file_name, 'wb')
+
+        #File header 14 bytes
+        bmp_file.write(char('B'))
+		bmp_file.write(char('M'))
+		bmp_file.write(dword(14 + 40 + self.width * self.height))
+		bmp_file.write(dword(0))
+		bmp_file.write(dword(14 + 40))
+
+        #File info 40 bytes
+        bmp_file.write(dword(40))
+		bmp_file.write(dword(self.width))
+		bmp_file.write(dword(self.height))
+		bmp_file.write(word(1))
+		bmp_file.write(word(24))
+		bmp_file.write(dword(0))
+		bmp_file.write(dword(self.width * self.height * 3))
+		bmp_file.write(dword(0))
+		bmp_file.write(dword(0))
+		bmp_file.write(dword(0))
+		bmp_file.write(dword(0))
+    
+        # Pixeles, 3 bytes each
+		for x in range(self.height):
+			for y in range(self.width):
+				bmp_file.write(self.pixels[x][y])
+        
+        bmp_file.close()
